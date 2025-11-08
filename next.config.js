2bf79d477055edd3ee0eb6392ff97b9c8295ae42
Shubham Khade <shubhkdesign@gmail.com>
2025-11-08 07:33:40 +0530

    payload with railway setup

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
         protocol: 'http',
       },
       {
-        hostname: process.env.NEXT_PUBLIC_SERVER_URL.split('https://')[1],
+        hostname: process.env.NEXT_PUBLIC_SERVER_URL[1],
         protocol: 'https',
       },
     ],
