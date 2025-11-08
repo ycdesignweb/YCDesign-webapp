761c6eda11cd8acba9d7304e338ad834e5ed3fb5
Shubham Khade <shubhkdesign@gmail.com>
2025-11-08 08:45:39 +0530

    s3 storage linked

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -85,7 +85,8 @@ export default buildConfig({
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
         },
         region: process.env.S3_REGION,
-        // ... Other S3 configuration
+        forcePathStyle: true,
+        endpoint: process.env.S3_ENDPOINT,
       },
     }),
   ],
